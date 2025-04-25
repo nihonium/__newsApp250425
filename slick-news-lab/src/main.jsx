9f18ed263d9cfc33802c1683c95fbbe9cc17d127
Hironori Niho <hironori103@gmail.com>
2025-04-25 20:21:38 +0900

    React Slickを追加

--- slick-news-lab/src/main.jsx
+++ slick-news-lab/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import SimpleSlider from './SimpleSlider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SimpleSlider />
   </StrictMode>,
 )
