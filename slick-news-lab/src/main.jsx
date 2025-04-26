a37fb929c42ab7ca1988e3cdf44e31e712545206
Hironori Niho <hironori103@gmail.com>
2025-04-26 17:09:32 +0900

    slickスライダーの修正

--- slick-news-lab/src/main.jsx
+++ slick-news-lab/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'reset-css'
 import './index.css'
-import SimpleSlider from './SimpleSlider.jsx'
+import CustomSlides from './CustomSlides.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SimpleSlider />
+    <CustomSlides />
   </StrictMode>,
 )
