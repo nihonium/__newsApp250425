6c387ca62492f87238dc93228485aee6af68fdbc
Hironori Niho <hironori103@gmail.com>
2025-04-26 16:34:13 +0900

    リセットCSSを追加 不要なstyleを削除

--- slick-news-lab/src/SimpleSlider.jsx
+++ slick-news-lab/src/SimpleSlider.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './App.css'
+import './SimpleSlider.css'
 import React from "react"
 import Slider from "react-slick"
 import "slick-carousel/slick/slick.css"
