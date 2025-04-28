0c3f1b456c1edc283742e365a75b9c93f32dbf29
Hironori Niho <hironori103@gmail.com>
2025-04-28 18:41:06 +0900

    コードリファクタ

--- slick-news-lab/src/main.jsx
+++ slick-news-lab/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import 'reset-css'
 import './index.css'
 import CustomSlides from './CustomSlides.jsx'
-// import './NewsAPI.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
